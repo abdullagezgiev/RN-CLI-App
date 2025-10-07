c0ed6604d0965ff7cbba5aa9a94ab93aaee942a0
abdullagezgiev <gezgievabdulla@gmail.com>
2025-10-07 21:12:56 +0600

    App.tsx is updated and new dependencies are installed

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {enableScreens} from 'react-native-screens';
 import ShiftListScreen from './src/context/ShiftListScreen';
 import ShiftDetailsScreen from './src/context/ShiftDetailsScreen';
-import { ShiftProvider } from './src/context/ShiftContext';
+import {ShiftProvider} from './src/context/ShiftContext';
+
+enableScreens(); 
 
 const Stack = createNativeStackNavigator();
 
@@ -12,21 +14,32 @@ function App(): React.JSX.Element {
   return (
     <ShiftProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName='ShiftList'>
-          <Stack.Screen
-            name="ShiftList"
+        <Stack.Navigator 
+          initialRouteName="ShiftList"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: '#f5f5f5',
+            },
+            headerTintColor: '#333',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        >
+          <Stack.Screen 
+            name="ShiftList" 
             component={ShiftListScreen}
             options={{title: 'Доступные смены'}}
           />
-          <Stack.Screen
-            name="ShiftDetails"
+          <Stack.Screen 
+            name="ShiftDetails" 
             component={ShiftDetailsScreen}
             options={{title: 'Детали смены'}}
           />
         </Stack.Navigator>
       </NavigationContainer>
-  </ShiftProvider>  
-  )
+    </ShiftProvider>
+  );
 }
 
 export default App;
